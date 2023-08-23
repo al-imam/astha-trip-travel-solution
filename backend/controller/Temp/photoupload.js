7be1ff819ab70ed4e44e130d724c7fe37dec601c
al-imam <alimam01828@gmail.com>
2023-08-23 11:36:52 +0600

    protect routes with isAuthenticate

--- backend/controller/Temp/photoupload.js
+++ backend/controller/Temp/photoupload.js
@@ -1,25 +1,21 @@
-const UploadTempPhoto = async (req,res)=>{
-
-
-
-const response = {
-    passpor:{
-        name:req.files?.imgpasport[0]?.filename
+const UploadTempPhoto = async (req, res) => {
+  const response = {
+    passpor: {
+      name: req.files?.imgpasport[0]?.filename,
     },
-    visa:{
-        name:req.files?.imgvisa? req.files?.imgvisa[0]?.filename : null
+    visa: {
+      name: req.files?.imgvisa ? req.files?.imgvisa[0]?.filename : null,
     },
-    hotelbooking:{
-        name: req.files?.hotel? req.files?.hotel[0]?.filename : null
+    hotelbooking: {
+      name: req.files?.hotel ? req.files?.hotel[0]?.filename : null,
     },
-    tiket:{
-        name: req.files?.ticket? req.files?.ticket[0]?.filename : null
-    }
-}
-    res.send({
-        ...response
-    })
-
-}
+    tiket: {
+      name: req.files?.ticket ? req.files?.ticket[0]?.filename : null,
+    },
+  };
+  res.send({
+    ...response,
+  });
+};
 
 module.exports = UploadTempPhoto;
