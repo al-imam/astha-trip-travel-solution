47e21f341a1ad7cde75790a50b211c1240582b82
al-imam <alimam01828@gmail.com>
2023-08-31 19:15:38 +0600

    create new loi approve-python route

--- backend/routers/LOI.js
+++ backend/routers/LOI.js
@@ -5,13 +5,22 @@ const GetAll = require("../controller/LOI/getall");
 const Approved = require("../controller/LOI/Approve");
 const CancelController = require("../controller/LOI/Cancel");
 const validateBody = require("../middleware/validator/validateBody");
-const { isString } = require("nested-object-validate");
+const { isString, isNumber } = require("nested-object-validate");
 const isAdmin = require("../middleware/Auth/isAdmin");
 const isAuthenticate = require("../middleware/Auth/isAuthenticate");
+const approveLoiRequest = require("../controller/LOI/approveLoiRequest");
 
 LOIRoute.post("/entry", isAuthenticate, LOIEntry);
 LOIRoute.get("/getall", isAdmin, GetAll);
 LOIRoute.post("/approved", isAdmin, Approved);
+
+LOIRoute.post(
+  "/approved-python",
+  isAdmin,
+  validateBody([isNumber("id", true)]),
+  approveLoiRequest
+);
+
 LOIRoute.post(
   "/cancel",
   isAdmin,
