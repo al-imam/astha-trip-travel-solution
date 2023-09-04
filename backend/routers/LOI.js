b38300c0c6c1da2c0303a55562d9ead98819baa8
al-imam <alimam01828@gmail.com>
2023-09-04 11:11:08 +0600

    protect pages

--- backend/routers/LOI.js
+++ backend/routers/LOI.js
@@ -2,7 +2,6 @@ const express = require("express");
 const LOIRoute = express.Router();
 const LOIEntry = require("../controller/LOI/LOIentry");
 const GetAll = require("../controller/LOI/getall");
-const Approved = require("../controller/LOI/Approve");
 const CancelController = require("../controller/LOI/Cancel");
 const validateBody = require("../middleware/validator/validateBody");
 const { isString, isNumber } = require("nested-object-validate");
@@ -12,7 +11,6 @@ const approveLoiRequest = require("../controller/LOI/approveLoiRequest");
 
 LOIRoute.post("/entry", isAuthenticate, LOIEntry);
 LOIRoute.get("/getall", isAdmin, GetAll);
-LOIRoute.post("/approved", isAdmin, Approved);
 
 LOIRoute.post(
   "/approved-python",
