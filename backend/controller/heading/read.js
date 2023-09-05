7173945dcc3d869fd8e08fb5b86b6f7ec2b476b4
al-imam <alimam01828@gmail.com>
2023-09-05 15:06:31 +0600

    implement get heading

---
+++ backend/controller/heading/read.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const { join } = require("path");
+
+const location = join(__dirname, "store.txt");
+
+async function read(req, res) {
+  try {
+    res.json({ text: fs.readFileSync(location, "utf-8") });
+  } catch (error) {
+    res.status(500).json({ message: "Error reading the file" });
+  }
+}
+
+module.exports = read;
