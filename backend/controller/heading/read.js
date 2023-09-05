4935296fb9267533a80fc1bc520b2336b44ae4db
al-imam <alimam01828@gmail.com>
2023-09-05 17:52:20 +0600

    fix read write error

--- backend/controller/heading/read.js
+++ backend/controller/heading/read.js
@@ -3,9 +3,17 @@ const { join } = require("path");
 
 const location = join(__dirname, "__store.txt");
 
-async function read(req, res) {
+function readFileSilent(p) {
   try {
-    res.json({ text: fs.readFileSync(location, "utf-8") });
+    return fs.readFileSync(p, "utf8").trim();
+  } catch (error) {
+    return "";
+  }
+}
+
+async function read(_, res) {
+  try {
+    res.json({ text: readFileSilent(location) });
   } catch (error) {
     res.status(500).json({ message: "Error reading the file" });
   }
--- backend/controller/heading/write.js
+++ backend/controller/heading/write.js
@@ -5,8 +5,8 @@ const location = join(__dirname, "__store.txt");
 
 async function write(req, res) {
   try {
-    fs.writeFileSync(location, req.body.text, "utf-8");
-    res.json({ success: true, text: req.body.text });
+    fs.writeFileSync(location, req.body.text.trim(), "utf-8");
+    res.json({ success: true, text: req.body.text.trim() });
   } catch (error) {
     res.status(500).json({ message: "Error updating the file" });
   }
