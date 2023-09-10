1e524ddaf81876139e1d230f69767b00b5f2da3a
al-imam <alimam01828@gmail.com>
2023-09-10 13:53:38 +0600

    create custom input for hook form

--- frontend/src/views/public/Entry/Schengen.jsx
+++ frontend/src/views/public/Entry/Schengen.jsx
@@ -1,3 +1,60 @@
+import { useId, useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+function Input({ register, error, label, ...rest }) {
+  const id = useId();
+  return (
+    <div className="space-y-1 text-sm">
+      <label htmlFor={id} className="block text-base font-medium text-gray-900">
+        {label}
+      </label>
+      <input
+        {...register}
+        id={id}
+        className={`block w-full rounded border-none bg-gray-50 p-2.5 text-gray-900 outline-none ring-1 focus:ring-2 ${
+          error ? "ring-red-500/50  focus:ring-red-500/50" : "ring-brand-100 focus:ring-blue-500/50"
+        }`}
+        placeholder={label}
+        {...rest}
+      />
+      {error && <p className="text-red-500/90">{error.message}</p>}
+    </div>
+  );
+}
+
+function Button({ children, ...rest }) {
+  return (
+    <button
+      className="inline-flex items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 "
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
+
+function NextIcon(props) {
+  return (
+    <svg
+      className="ml-2 h-3.5 w-3.5"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 14 10"
+      {...props}
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M1 5h12m0 0L9 1m4 4L9 9"
+      />
+    </svg>
+  );
+}
+
 function StepIndicator({ steps = [], current = 2 }) {
   return (
     <ol className="flex w-full items-center justify-center gap-2 text-sm font-medium text-gray-500 sm:space-x-4 sm:text-base">
