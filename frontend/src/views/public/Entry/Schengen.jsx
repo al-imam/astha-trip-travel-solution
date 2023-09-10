aec1d7a386401a23432196e22fa0baac45e3c021
al-imam <alimam01828@gmail.com>
2023-09-10 20:07:20 +0600

    create necessary fields

--- frontend/src/views/public/Entry/Schengen.jsx
+++ frontend/src/views/public/Entry/Schengen.jsx
@@ -1,13 +1,22 @@
 import { Button } from "components/form/Button";
-import { StepIndicator } from "components/form/StepIndicator";
 import { Input } from "components/form/Input";
+import { StepIndicator } from "components/form/StepIndicator";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import countries from "../countries.json";
+import { Select } from "components/form/Select";
+
+const countriesOptions = countries.map((e) => ({
+  label: e.name,
+  value: e.name.toUpperCase(),
+}));
 
 export function Schengen() {
   const personal = useForm({ mode: "onSubmit" });
   const [step, setStep] = useState(1);
+
+  console.log(personal.formState.errors);
 
   return (
     <main className="container mx-auto space-y-4 p-4">
@@ -18,38 +27,58 @@ export function Schengen() {
         Back to dashboard
       </Link>
 
-      <div className="space-y-4 rounded border border-gray-200 bg-white px-4 py-8 shadow-sm sm:p-4">
+      <div className="space-y-4 rounded border border-gray-200 bg-white px-4 py-8 shadow-sm ">
         <StepIndicator steps={["Personal", "Travel Document", "Contact and Occupation"]} current={step} />
 
         {step === 1 && (
           <form className="space-y-4" onSubmit={personal.handleSubmit(() => setStep(2))}>
-            <Input
-              label="Surname"
-              register={personal.register("surname", { required: "Surname is required" })}
-              error={personal.formState.errors["surname"]}
-            />
+            <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+              <Input
+                label="Surname"
+                register={personal.register("surname", { required: "Surname is required" })}
+                error={personal.formState.errors["surname"]}
+                type="text"
+              />
 
-            <Input
-              label="Surname at birth"
-              register={personal.register("surname-at-birth", { required: "Surname at birth is required" })}
-              error={personal.formState.errors["surname-at-birth"]}
-            />
+              <Input
+                label="Surname at birth"
+                register={personal.register("surname-at-birth", { required: "Surname at birth is required" })}
+                error={personal.formState.errors["surname-at-birth"]}
+                type="text"
+              />
 
-            <Input
-              label="Fist name"
-              register={personal.register("first-name", { required: "Fist name is required" })}
-              error={personal.formState.errors["first-name"]}
-            />
+              <Input
+                label="Fist name"
+                register={personal.register("first-name", { required: "Fist name is required" })}
+                error={personal.formState.errors["first-name"]}
+                type="text"
+              />
 
-            <Input
-              label="Date of birth"
-              register={personal.register("date-of-birth", {
-                required: "Birth date is required",
-                max: { value: new Date().toISOString().split("T")[0], message: "Birth date cannot be future date" },
-              })}
-              error={personal.formState.errors["date-of-birth"]}
-              type="date"
-            />
+              <Input
+                label="Date of birth"
+                register={personal.register("date-of-birth", {
+                  required: "Birth date is required",
+                  max: { value: new Date().toISOString().split("T")[0], message: "Birth date cannot be future date" },
+                })}
+                error={personal.formState.errors["date-of-birth"]}
+                type="date"
+              />
+              <Input
+                label="Place of birth"
+                register={personal.register("place-of-birth", { required: "Place of birth is required" })}
+                error={personal.formState.errors["place-of-birth"]}
+                type="text"
+              />
+
+              <Select
+                label="Country of birth"
+                options={countriesOptions}
+                control={personal.control}
+                name="country-of-birth"
+                register={personal.register("country-of-birth", { required: "Country of birth is required" })}
+                error={personal.formState.errors["country-of-birth"]}
+              />
+            </div>
 
             <div className="flex justify-end">
               <Button>
