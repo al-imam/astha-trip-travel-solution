97801e3a00b49f1d753a30e1a056ccd3f84d8590
al-imam <alimam01828@gmail.com>
2023-09-11 13:44:57 +0600

    multistep data persist local

--- frontend/src/views/public/Entry/Schengen.jsx
+++ frontend/src/views/public/Entry/Schengen.jsx
@@ -1,22 +1,47 @@
 import { Button } from "components/form/Button";
 import { Input } from "components/form/Input";
+import { Select } from "components/form/Select";
 import { StepIndicator } from "components/form/StepIndicator";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import useFormPersist from "react-hook-form-persist";
 import { Link } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
 import countries from "../countries.json";
-import { Select } from "components/form/Select";
 
 const countriesOptions = countries.map((e) => ({
   label: e.name,
   value: e.name.toUpperCase(),
 }));
 
+const steps = ["Personal", "Travel Document", "Contact and Occupation"];
+
 export function Schengen() {
-  const personal = useForm({ mode: "onSubmit" });
+  const personal = useForm();
+  const travel = useForm();
   const [step, setStep] = useState(1);
 
-  console.log(personal.formState.errors);
+  const clearPersonal = useFormPersist("personal-visa-data", {
+    watch: personal.watch,
+    setValue: personal.setValue,
+    storage: window.localStorage,
+    validate: true,
+  });
+
+  const clearTravel = useFormPersist("travel-visa-data", {
+    watch: travel.watch,
+    setValue: travel.setValue,
+    storage: window.localStorage,
+    validate: true,
+  });
+
+  function personalInfoSubmit(data) {
+    setStep(2);
+  }
+
+  function travelInfoSubmit(data) {
+    setStep(3);
+  }
 
   return (
     <main className="container mx-auto space-y-4 p-4">
@@ -28,10 +53,10 @@ export function Schengen() {
       </Link>
 
       <div className="space-y-4 rounded border border-gray-200 bg-white px-4 py-8 shadow-sm ">
-        <StepIndicator steps={["Personal", "Travel Document", "Contact and Occupation"]} current={step} />
+        <StepIndicator steps={steps} current={step} />
 
         {step === 1 && (
-          <form className="space-y-4" onSubmit={personal.handleSubmit(() => setStep(2))}>
+          <form className="space-y-4" onSubmit={personal.handleSubmit(personalInfoSubmit)}>
             <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
               <Input
                 label="Surname"
@@ -87,6 +112,67 @@ export function Schengen() {
             </div>
           </form>
         )}
+
+        {step === 2 && (
+          <form className="space-y-4" onSubmit={travel.handleSubmit(travelInfoSubmit)}>
+            <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+              <Input
+                label="Surname"
+                register={travel.register("surname", { required: "Surname is required" })}
+                error={travel.formState.errors["surname"]}
+                type="text"
+              />
+
+              <Input
+                label="Surname at birth"
+                register={travel.register("surname-at-birth", { required: "Surname at birth is required" })}
+                error={travel.formState.errors["surname-at-birth"]}
+                type="text"
+              />
+
+              <Input
+                label="Fist name"
+                register={travel.register("first-name", { required: "Fist name is required" })}
+                error={travel.formState.errors["first-name"]}
+                type="text"
+              />
+
+              <Input
+                label="Date of birth"
+                register={travel.register("date-of-birth", {
+                  required: "Birth date is required",
+                  max: { value: new Date().toISOString().split("T")[0], message: "Birth date cannot be future date" },
+                })}
+                error={travel.formState.errors["date-of-birth"]}
+                type="date"
+              />
+              <Input
+                label="Place of birth"
+                register={travel.register("place-of-birth", { required: "Place of birth is required" })}
+                error={travel.formState.errors["place-of-birth"]}
+                type="text"
+              />
+
+              <Select
+                label="Country of birth"
+                options={countriesOptions}
+                control={travel.control}
+                name="country-of-birth"
+                register={travel.register("country-of-birth", { required: "Country of birth is required" })}
+                error={travel.formState.errors["country-of-birth"]}
+              />
+            </div>
+
+            <div className="flex justify-between">
+              <Button type="button" onClick={() => setStep(1)}>
+                <NextIcon className="mr-2 scale-x-[-1]" /> Previous
+              </Button>
+              <Button>
+                Next <NextIcon className="ml-2" />
+              </Button>
+            </div>
+          </form>
+        )}
       </div>
     </main>
   );
@@ -103,15 +189,15 @@ export function LeftArrow(props) {
   );
 }
 
-function NextIcon(props) {
+function NextIcon({ className, ...rest }) {
   return (
     <svg
-      className="ml-2 h-3.5 w-3.5"
+      className={twMerge(" h-3.5 w-3.5", className)}
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 14 10"
-      {...props}
+      {...rest}
     >
       <path
         stroke="currentColor"
